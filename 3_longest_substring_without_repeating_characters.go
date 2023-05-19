8401bac98efd2c335308989acb4183e1196ab41d
hi6okuni <hi6okuni@gmail.com>
2023-05-20 00:54:01 +0900

    feat: add alternative answer of 3

--- 3_longest_substring_without_repeating_characters.go
+++ 3_longest_substring_without_repeating_characters.go
@@ -32,6 +32,29 @@ func lengthOfLongestSubstring(s string) int {
 }
 
 // cool approach
+func lengthOfLongestSubstring2(s string) int {
+	if len(s) < 2 {
+		return len(s)
+	}
+	l, r, max := 0, 0, 0
+	dict := make(map[byte]bool)
+	for r < len(s) {
+		// rが右方向に進んでdict[s[r]]が重複が発生したら、
+		// lをfalseで右方向に順番に塗りつぶしていき
+		// lが重複した文字まで辿り着くとdict[s[r]]が再びfalseに変わる
+		if dict[s[r]] == true {
+			dict[s[l]] = false
+			l++
+		} else {
+			dict[s[r]] = true
+			if (r-l)+1 > max {
+				max = (r - l) + 1
+			}
+			r++
+		}
+	}
+	return max
+}
 
 /*
 Given a string s, find the length of the longest
