d28c0fd4c1e8a937048b7a84f29a79142e71a88d
hhonda <hhonda@SS2206010001.local>
2023-07-26 10:11:47 +0900

    edit 121

--- 121_best_time_to_buy_and_sell_stock.go
+++ 121_best_time_to_buy_and_sell_stock.go
@@ -4,10 +4,7 @@ package main
 func maxProfitTooSlow(prices []int) int {
 	ans := 0
 	for i := 0; i < len(prices); i++ {
-		for j := 1; j < len(prices)-1; j++ {
-			if i >= j {
-				continue
-			}
+		for j := i + 1; j < len(prices); j++ {
 			if (prices[j] - prices[i]) > ans {
 				ans = prices[j] - prices[i]
 			}
