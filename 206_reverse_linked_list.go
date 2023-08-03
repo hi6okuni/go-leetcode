e6cd985da570973308cfda85d3238da7a518ef72
hi6okuni <hi6okuni@gmail.com>
2023-08-03 18:19:31 +0900

    add 2 question

---
+++ 206_reverse_linked_list.go
@@ -0,0 +1,36 @@
+package main
+
+func reverseList(head *ListNode) *ListNode {
+	return recursive(head, nil)
+}
+
+func recursive(curr, prev *ListNode) *ListNode {
+	if curr == nil {
+		return prev
+	}
+	next := curr.Next
+	curr.Next = prev
+	return recursive(next, curr)
+}
+
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Example 1:
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+// Example 2:
+
+// Input: head = [1,2]
+// Output: [2,1]
+// Example 3:
+
+// Input: head = []
+// Output: []
+
+// Constraints:
+
+// The number of nodes in the list is the range [0, 5000].
+// -5000 <= Node.val <= 5000
+
+// Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
---
+++ 322_coin_change.go
@@ -0,0 +1,80 @@
+package main
+
+// func coinChange(coins []int, amount int) int {
+// 	sort.Ints(coins)
+// 	var rest = amount
+// 	var num int
+// 	if rest == 0 {
+// 		return 0
+// 	}
+
+// 	for i := 0; i < len(coins); i++ {
+// 		if rest < coins[len(coins)-i-1] {
+// 			continue
+// 		}
+// 		for rest/coins[len(coins)-i-1] > 0 {
+// 			rest -= coins[len(coins)-i-1]
+// 			num++
+// 		}
+// 		if rest == 0 {
+// 			return num
+// 		}
+// 	}
+
+// 	return -1
+// }
+
+func coinChange(coins []int, amount int) int {
+	// we initialize dp with amount + 1 because it's the worst case
+	dp := make([]int, amount+1)
+	for i := range dp {
+		dp[i] = amount + 1
+	}
+	dp[0] = 0
+
+	for i := 0; i < len(coins); i++ {
+		for j := coins[i]; j <= amount; j++ {
+			// here we compare the current dp[j] with a new calculated value
+			dp[j] = min(dp[j], dp[j-coins[i]]+1)
+		}
+	}
+
+	// return -1 when no combination can sum to amount
+	if dp[amount] > amount {
+		return -1
+	}
+	return dp[amount]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+// Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+// You may assume that you have an infinite number of each kind of coin.
+
+// Example 1:
+
+// Input: coins = [1,2,5], amount = 11
+// Output: 3
+// Explanation: 11 = 5 + 5 + 1
+// Example 2:
+
+// Input: coins = [2], amount = 3
+// Output: -1
+// Example 3:
+
+// Input: coins = [1], amount = 0
+// Output: 0
+
+// Constraints:
+
+// 1 <= coins.length <= 12
+// 1 <= coins[i] <= 231 - 1
+// 0 <= amount <= 104
